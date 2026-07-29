05187e7b15e5c694a2fcda850c946d5fbb0484e1
Pieter Ayal <pietera.mullock@gmail.com>
2026-07-29 22:53:07 +0100

    Finished ex4! No flipping way..

--- maelstrom-g-counter/main.go
+++ maelstrom-g-counter/main.go
@@ -1,39 +1,97 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"log"
+	"os"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-func main() {
-	n := maelstrom.NewNode()
+var singleKey string = "g-counter"
 
-	n.Handle("add", func(msg maelstrom.Message) error {
+func main() {
+	node := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(node)
+
+	node.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 		delta := int(body["delta"].(float64))
 
-		// Increment value of a single global counter by delta.
+		for {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			read, err := kv.ReadInt(ctx, singleKey)
+			cancel()
+			if err != nil {
+				var rpcErr *maelstrom.RPCError
+				if errors.As(err, &rpcErr) && rpcErr.Code == maelstrom.KeyDoesNotExist {
+					read = 0 // key hasn't been created yet — treat current value as 0
+				} else {
+					time.Sleep(20 * time.Millisecond)
+					continue
+				}
+			}
 
-		return n.Reply(msg, map[string]any{
+			newVal := delta + read
+			ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+			err = kv.CompareAndSwap(ctx, singleKey, read, newVal, true)
+			cancel()
+			if err != nil {
+				time.Sleep(20 * time.Millisecond)
+				continue
+			}
+			break
+		}
+
+		return node.Reply(msg, map[string]any{
 			"type": "add_ok",
 		})
-
 	})
 
-	n.Handle("read", func(msg maelstrom.Message) error {
+	node.Handle("read", func(msg maelstrom.Message) error {
 		/*
 			Return the current value of the global counter.
-			Remember that the counter service is only sequentially consistent.
+			Remember that the counter service is sequentially consistent.
 		*/
-		value := 1234 // placeholder
+		for {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			value, err := kv.ReadInt(ctx, singleKey)
+			cancel()
+			if err != nil {
+				var rpcErr *maelstrom.RPCError
+				if errors.As(err, &rpcErr) && rpcErr.Code == maelstrom.KeyDoesNotExist {
+					value = 0 // key hasn't been created yet — treat current value as 0
+				} else {
+					time.Sleep(20 * time.Millisecond)
+					continue
+				}
+			}
 
-		return n.Reply(msg, map[string]any{
-			"type":  "read_ok",
-			"value": value,
-		})
+			ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+			err = kv.CompareAndSwap(ctx, singleKey, value, value, true)
+			cancel()
+			if err != nil {
+				time.Sleep(20 * time.Millisecond)
+				continue
+			}
+
+			return node.Reply(msg, map[string]any{
+				"type":  "read_ok",
+				"value": value,
+			})
+		}
+
 	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
